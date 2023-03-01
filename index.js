73cfa0afcc4f96ad13fa83d169156189c2e7c094
Hrushikesh Surkar <112410741+HrushikeshSurkar@users.noreply.github.com>
2023-03-01 20:51:13 +0530

    connected with cloud
    MongoDB Cloud connection successful

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const url = 'mongodb://localhost/menudb'
 
+const url = 'mongodb+srv://Hrushikesh:Hrushikesh_123@cluster0.wnkx44y.mongodb.net/menu?retryWrites=true&w=majority'
 const app = express()
-// mongoose.set('strictQuery', false);
-mongoose.connect(url, { useNewUrlParser: true })
+mongoose.set('strictQuery', false);
+mongoose.connect(url, { useNewUrlParser: true }).then(() => {
+    console.log("connection Successfully");
+}).catch((err) => console.log("no connection"));
 const con = mongoose.connection
 
 con.on('open', () => {
